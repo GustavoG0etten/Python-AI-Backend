cbc32fe4db34b848be263faeb677f9d82b044e88
GustavoG0etten <gustavogoetten87@gmail.com>
2024-03-28 17:23:18 -0300

    :construction: primeiros comandos em python

---
+++ entrada_saida_dados.py
@@ -0,0 +1,7 @@
+nome = input("Seu nome: ")
+idade = input("Sua idade: ")
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Gustavo"
+idade = 20
+
+# nome = "Mikael"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+STATES = ["SC", "PR", "MT", "SP", "RJ", "DF"]
+
+print(STATES)
